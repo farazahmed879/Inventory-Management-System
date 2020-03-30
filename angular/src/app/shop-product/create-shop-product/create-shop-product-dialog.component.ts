35798f4b4ec5585505b5842547f5ad4445576c37
Uzair Iqbal <uzair.qq@outlook.com>
2020-03-31 02:29:26 +0500

    Shop Product Added Two Dropdown in Add

--- angular/src/app/shop-product/create-shop-product/create-shop-product-dialog.component.ts
+++ angular/src/app/shop-product/create-shop-product/create-shop-product-dialog.component.ts
@@ -1,27 +1,31 @@
-import { Component, Injector, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
-import { finalize } from 'rxjs/operators';
-import { AppComponentBase } from '@shared/app-component-base';
+import { Component, Injector, OnInit } from "@angular/core";
+import { MatDialogRef } from "@angular/material";
+import { finalize } from "rxjs/operators";
+import { AppComponentBase } from "@shared/app-component-base";
 import {
   ShopProductServiceServiceProxy,
   CreateShopProductDto,
   CompanyServiceServiceProxy,
   CompanyDto,
   ProductServiceServiceProxy,
-  ProductDto
-} from '@shared/service-proxies/service-proxies';
+  ProductDto,
+  TypeServiceServiceProxy,
+  SubTypeServiceServiceProxy,
+  SubTypeDto,
+  TypeDto
+} from "@shared/service-proxies/service-proxies";
 
 @Component({
-  templateUrl: 'create-shop-product-dialog.component.html',
+  templateUrl: "create-shop-product-dialog.component.html",
   styles: [
     `
-            mat-form-field {
-                width: 100%;
-            }
-            mat-checkbox {
-                padding-bottom: 5px;
-            }
-        `
+      mat-form-field {
+        width: 100%;
+      }
+      mat-checkbox {
+        padding-bottom: 5px;
+      }
+    `
   ]
 })
 export class CreateShopProductDialogComponent extends AppComponentBase
@@ -30,12 +34,18 @@ export class CreateShopProductDialogComponent extends AppComponentBase
   shopProduct: CreateShopProductDto = new CreateShopProductDto();
   companies: CompanyDto[];
   products: ProductDto[];
+  subTypes: SubTypeDto[] = [];
+  types: TypeDto[] = [];
+  selectedSubTypeId: string;
+  selectedTypeId: string;
 
   constructor(
     injector: Injector,
     public _shopProductService: ShopProductServiceServiceProxy,
     public _productService: ProductServiceServiceProxy,
     public _companyService: CompanyServiceServiceProxy,
+    private _typeService: TypeServiceServiceProxy,
+    private _subTypeService: SubTypeServiceServiceProxy,
     private _dialogRef: MatDialogRef<CreateShopProductDialogComponent>
   ) {
     super(injector);
@@ -44,7 +54,10 @@ export class CreateShopProductDialogComponent extends AppComponentBase
   ngOnInit(): void {
     this.getAllProduct();
     this.getAllCompany();
+    this.getAllSubTypes();
+    this.getAllTypes();
   }
+
   getAllCompany() {
     this._companyService.getAll().subscribe(result => {
       this.companies = result;
@@ -54,6 +67,18 @@ export class CreateShopProductDialogComponent extends AppComponentBase
   getAllProduct() {
     this._productService.getAll().subscribe(result => {
       this.products = result;
+    });
+  }
+
+  getAllTypes() {
+    this._typeService.getAll().subscribe(result => {
+      this.types = result;
+    });
+  }
+
+  getAllSubTypes() {
+    this._subTypeService.getAll().subscribe(result => {
+      this.subTypes = result;
     });
   }
 
@@ -68,7 +93,7 @@ export class CreateShopProductDialogComponent extends AppComponentBase
         })
       )
       .subscribe(() => {
-        this.notify.info(this.l('SavedSuccessfully'));
+        this.notify.info(this.l("SavedSuccessfully"));
         this.close(true);
       });
   }
